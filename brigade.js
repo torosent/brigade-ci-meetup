1d25a2001213861be484df50519c0ef61a33bf1a
torosent <torosent@microsoft.com>
2018-01-24 11:31:03 +0200

    updated github status

--- brigade.js
+++ brigade.js
@@ -4,31 +4,36 @@ const {
 } = require("brigadier");
 
 events.on("push", function (e, p) {
-    console.log("received push for commit " + e.commit)
+  console.log("received push for commit " + e.commit)
 
-    // Create a new job
-    var node = new Job("test-runner")
-    node.image = "python:3"
+  // Create a new job
+  var node = new Job("test-runner")
+  node.image = "python:3"
 
-    node.tasks = [
-      "cd /src/app",
-      "pip install -r requirements.txt",
-      "cd /src/",
-      "python setup.py test"
-    ]
+  node.tasks = [
+    "cd /src/app",
+    "pip install -r requirements.txt",
+    "cd /src/",
+    "python setup.py test"
+  ]
 
-    node.run().then(() => {
-
-      var gh = new Job("gh");
-      gh.image = "technosophos/github-notify:latest"
-      gh.env = {
-        GH_REPO: p.repo.name,
-        GH_STATE: "success",
-        GH_DESCRIPTION: "brigade says YES!",
-        GH_CONTEXT: "brigade",
-        GH_TOKEN: p.repo.token,
-        GH_COMMIT: e.commit,
-      };
-      gh.run().then(() => console.log("Status updated."));
-    });
+  node.run();
 });
+
+events.on("after", function (e, project) {
+  var c = e.cause.event
+  var m = "Hook " + c.type + " is in state " + e.cause.trigger +
+    " for build " + e.commit + " of " + project.repo.name;
+
+  var gh = new Job("gh");
+  gh.image = "technosophos/github-notify:latest"
+  gh.env = {
+    GH_REPO: p.repo.name,
+    GH_STATE: e.cause.trigger,
+    GH_DESCRIPTION: "brigade says YES!",
+    GH_CONTEXT: "brigade",
+    GH_TOKEN: p.repo.token,
+    GH_COMMIT: e.commit,
+  };
+  gh.run().then(() => console.log("Status updated: " + m ));
+})
